7067d2ada0f6ed73410e08d369301248e4a607dd
pravalika0 <93221515+pravalika0@users.noreply.github.com>
2024-06-16 23:02:40 +0530

    Add files via upload

---
+++ src/components/Projects/index.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { useState } from "react";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Desc,
+  CardContainer,
+  ToggleButtonGroup,
+  ToggleButton,
+  Divider,
+} from "./ProjectsStyle";
+import ProjectCard from "../Cards/ProjectCards";
+import { projects } from "../../data/constants";
+
+const Projects = ({ openModal, setOpenModal }) => {
+  const [toggle, setToggle] = useState("all");
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. Where I applied different
+          skills:
+        </Desc>
+        <ToggleButtonGroup>
+          {toggle === "all" ? (
+            <ToggleButton active value="all" onClick={() => setToggle("all")}>
+              All
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="all" onClick={() => setToggle("all")}>
+              All
+            </ToggleButton>
+          )}
+          <Divider />
+          {toggle === "web app" ? (
+            <ToggleButton
+              active
+              value="web app"
+              onClick={() => setToggle("web app")}
+            >
+              Web app's
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="web app" onClick={() => setToggle("web app")}>
+              Web app's
+            </ToggleButton>
+          )}
+          <Divider />
+          {toggle === "android app" ? (
+            <ToggleButton
+              active
+              value="android app"
+              onClick={() => setToggle("android app")}
+            >
+              Robotics
+            </ToggleButton>
+          ) : (
+            <ToggleButton
+              value="android app"
+              onClick={() => setToggle("android app")}
+            >
+              Robotics
+            </ToggleButton>
+          )}
+          <Divider />
+          {toggle === "machine learning" ? (
+            <ToggleButton
+              active
+              value="machine learning"
+              onClick={() => setToggle("machine learning")}
+            >
+              Augmented Reality{" "}
+            </ToggleButton>
+          ) : (
+            <ToggleButton
+              value="machine learning"
+              onClick={() => setToggle("machine learning")}
+            >
+              Augmented Reality
+            </ToggleButton>
+          )}
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === "all" &&
+            projects.map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Projects;
---
+++ src/data/constants.js
@@ -0,0 +1,271 @@
+export const Bio = {
+  name: "Pravalika Gandhamalla",
+  roles: ["Developer", "Programmer", "Dancer"],
+  description:
+    "I am a motivated and enthusiastic B.Tech student with a diverse set of skills and a passion for technology. As a fresher, I am eager to connect with professionals in the industry and explore job opportunities that allow me to contribute, learn, and grow. I am ready to leverage my knowledge and abilities in a dynamic work environment to make a meaningful impact. Let's connect and discuss how I can be a valuable addition to your team!",
+  github: "https://github.com/pravalika0",
+  resume:
+    "https://drive.google.com/file/d/1f96KUR1X57enWdfheuGoyninCTdnepaA/view?usp=drivesdk",
+  linkedin:
+    "https://www.linkedin.com/in/pravalika-g?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  twitter:
+    "https://www.instagram.com/sweety_gandhamalla?igsh=MWlvZmYxeHA2OGN4cA==",
+  insta:
+    "https://www.instagram.com/sweety_gandhamalla?igsh=MWlvZmYxeHA2OGN4cA==",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "PHP",
+        image: "https://images.app.goo.gl/uQ9BYLHawXPrxhY86",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "C++",
+        image: "https://logowik.com/content/uploads/images/321_c.jpg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://tolleson.com/wp-content/uploads/2017/07/cisco-brand-logo-blue-light-background.png",
+    role: "Mentee",
+    company: "CISCO",
+    program: "BOOST 4.0 Program",
+    date: "May 2024 - Current",
+    desc: "Attending the Cisco CWiCS India BOOST 4.0 Program which is a 45-day initiative dedicated to empowering women in cybersecurity through a blend of classroom training and self-paced learning.",
+    skills: [
+      "Cybersecurity fundamentals",
+      "Network security",
+      "Network defence",
+      "Network configuration",
+      "Pyhton",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/D560BAQGhGl9gZ5NfjA/company-logo_200_200/0/1692156655367?e=2147483647&v=beta&t=OZb3fdzIfTqbhfYvWMMv2KtokQJN6f__-wxcE2pMADQ",
+    role: "Mentee",
+    company: "Smart Interviews",
+    date: "Sept 2023 - April 2024",
+    desc: "As a mentee in the comprehensive Data Structures and Algorithms (DSA) course from Smart Interviews, I mastered all DSA topics and solved real-world problems to solidify my understanding.",
+    skills: [
+      "Problem solving",
+      "Analytical thinking",
+      "Coding & debugging",
+      "Java",
+    ],
+    doc: "https://smartinterviews.in/certificate/04ad2392",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSyEvgdM7MsMLbjLvKeUVJvEZJj5Ko62vCaQ&s",
+    role: "Mentee",
+    company: "Micron",
+    date: "May 2023 - April 2023",
+    desc: "As a mentee in the Micron Global Mentorship Program, I was paired with a mentor and participated in live sessions that guided us in transitioning professional skills from college to the workplace, focusing on leadership, critical thinking, decision-making, and more.",
+    skills: [
+      "Professional development",
+      "Leadership",
+      "Networking ",
+      "Effective communication and teamwork",
+      "Critical thinking",
+      "Problem solving",
+      "Decision-making",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/C4D0BAQHUP8La026ivA/company-logo_200_200/0/1630576857521/interactive_tutorials_logo?e=2147483647&v=beta&t=zvz6q8dbR1Dx6gsV8NWQE8oYXKpAtpRx4BtFBn-dcBs",
+    role: "Makerting Management Intern",
+    company: "LearnX",
+    date: "Oct 2022 - Dec 2022",
+    desc: "Tasked with developing a static website to host study materials and mentor live sessions, and then selling it to customers.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Networking ",
+      "Tramwork",
+      "Marketing",
+      "Leadership",
+    ],
+    doc: "https://drive.google.com/file/d/1NR-SbPmBf2Gces2bRcOfhevMVnMUlNpr/view?usp=drivesdk",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStZz8lMVn-emVLv1mgXy0QtAdIovEozE8EtQ&s",
+    school: "G. Narayanamma Institute of Technology Science, Hyderabad",
+    date: "Oct 2021 - June 2025",
+    grade: "8.77 CGPA",
+    desc: "I am currently pursuing a B.Tech in Computer Science Engineering at G. Narayanamma Institute of Technology and Science, Hyderabad, having completed my third year. Over the past three years, I have gained knowledge in various concepts such as OOP, DAA, DSA, DBMS, OS, and COA. Additionally, I serve as the Finance Chair for our college's dance club, EvenOdds, and am an active member of the Google Developer Student Club (GDSC), where I learn new technologies and build projects.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShgQ2F99Rk5_ti5ArDtQRDZX-w-hZfEqngyQ&s",
+    school: "Trividyaa Junior College,Suchitra",
+    date: "June 2019 - July 2021",
+    grade: "98%",
+    desc: "I completed my intermediate at Trividyaa Junior College, where I opted MPC as my main stream.",
+    degree: "Telangana State Board of Intermediate Education",
+  },
+  {
+    id: 2,
+    img: "https://img1.wsimg.com/isteam/ip/4c6402e0-7791-4245-a22a-7f2d32055b83/Address%20-%20Copy%20LOGO.png/:/cr=t:2.44%25,l:0%25,w:100%25,h:93.49%25/rs=w:600,h:600,cg:true",
+    school: "St.Ann's High School, Pet-Basheerabad",
+    date: "Apr 2019",
+    grade: "78.4%",
+    desc: "I completed my Class 10 education at St. Ann's High School, where, along with academics, I actively participated in co-curricular activities. I earned recognition for my achievements by receiving trophies and medals.",
+    degree: "Indian Certificate of Secondary Education",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "VisuLearn",
+    date: "Dec 2023 - current",
+    description:
+      "Developed a mobile application offers an immersive and interactive learniing experience,revolutionizing traditional education methods",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3U9rUx8mAnKYCsVEzIxgQH9EDfVf6tcQAoA&s",
+    tags: ["React Native", "Node Js"],
+    category: "machine learning",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 2,
+    title: "DigitalAR Menu",
+    date: "Jan 2024 - current",
+    description:
+      "Developing an augmented reality (AR) digital menu that allows customers to scan a QR code and view a list of food items with their real-life appearance, as prepared by the chef. This innovative menu enhances the dining experience by providing a visual representation of each dish, helping customers make informed choices.",
+    image:
+      "https://imgix.cosmicjs.com/4cf584e0-125d-11ec-b1d5-4d3fd4454d55-Image-1.png",
+    tags: ["React", "JavaScript", "QR code integration"],
+    category: "machine learning",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 3,
+    title: "Math For All",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "I developed a basic quiz webpage featuring a timer for each question and immediate feedback on incorrect answers, displaying the correct one. The quiz includes multiple levels, with increasing difficulty. At the end of the quiz, the winner is displayed, with points calculated based on response time.",
+    image:
+      "https://png.pngtree.com/png-clipart/20220502/original/pngtree-quiz-game-landing-web-page-header-banner-template-vector-png-image_7652635.png",
+    tags: ["HTML", "CSS", "Javascript"],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 10,
+    title: "Mobility of Drones",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Led a team to develop drone mobility system for efficient item delivery",
+    image:
+      "https://bsmedia.business-standard.com/_media/bs/img/article/2021-09/14/full/1631640768-5349.jpg",
+    tags: ["Mission Planner"],
+    category: "android app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 5,
+    title: "Duty Roaster",
+    date: "Dec 2023 - March 2024",
+    description:
+      "I developed a to-do list webpage that allows users to list activities, prioritize tasks, and receive alerts. The app summarizes travel distances and efficiently manages daily routines.",
+    image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--exyTD2H2--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_800/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/tbntxrja7xe4jfusyq62.PNG",
+    tags: ["React Js", "JavaScript", "Node JS"],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+];
